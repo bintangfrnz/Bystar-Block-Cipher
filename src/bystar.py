ad4d9fb631d3b05a73c81379055be6aa95a04eb4
Bintang Fajarianto <13519138@std.stei.itb.ac.id>
2023-04-07 07:59:17 +0700

    feat: add requirement constant

---
+++ src/bystar.py
@@ -0,0 +1,17 @@
+# Bintang Fajarianto
+# NIM 13519138
+
+# March 5, 2023
+
+class Bystar:
+  def generate_internal_key(self):
+    return
+  
+  def encrypt(self):
+    return
+  
+  def decrypt(self):
+    return
+  
+  def rotation(self):
+    return
---
+++ src/constant.py
@@ -0,0 +1,10 @@
+# Bintang Fajarianto
+# NIM 13519138
+
+# March 5, 2023
+
+ITERATION = 16 # round
+BLOCK_SIZE = 128 # bits
+
+PLAIN_TEXT = "This Bystar cipher created by Bintang NIM (13519138) on March 5." # 64 bytes (4 blocks)
+KEY = "Bystar138Cipherz" # 16 bytes
